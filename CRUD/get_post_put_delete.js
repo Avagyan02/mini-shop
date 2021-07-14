429527ec77e359440a91b47b7125f128d2c95fe7
Avagyan02 <samvel.avagyan.02@bk.ru>
2021-07-14 08:29:21 +0400

    update_Category

---
+++ CRUD/get_post_put_delete.js
@@ -0,0 +1,56 @@
+const connect = require('../moduls/connect');
+const app = require('../moduls/express');
+const Category = require('../models/schema');
+
+let newCat = {};
+
+app.post('/category', create);
+
+function create(req,res){
+  Category
+    .create({nameEn: req.body.nameEn, nameRu: req.body.nameRu, nameHy: req.body.nameHy})
+    .then( category => {
+      console.log('Category saved');
+      res.status(200).json({
+        "success": true,
+        "message": "Category created",
+        "data": {
+          nameEn: req.body.nameEn,
+          nameRu: req.body.nameRu,
+          nameHy: req.body.nameHy,
+          productCount: 0
+        }
+      })
+    })
+    .catch(err => {
+      console.log(err);
+      res.send('Category not saved');
+    })
+}
+
+app.put('/category/:id', (req,res) => {
+  Category.findOneAndUpdate({_id: req.params.id},{  
+    $set: {
+      nameEn: req.body.nameEn, 
+      nameRu: req.body.nameRu, 
+      nameHy: req.body.nameHy,
+    }
+  })
+  .then(result =>  {
+    let category = Category.find({_id: req.params.id});
+    res.status(200).json({
+      "success": true,
+      "message": "Category updated",
+      "data": {
+        _id: req.params.id,
+        nameEn: req.body.nameEn,
+        nameRu: req.body.nameRu,
+        nameHy: req.body.nameHy,
+        productCount: 0
+      }
+    })
+  })
+  .catch(err => {
+    console.log(err);
+  })
+})
