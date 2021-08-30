5d8a048266006998a20c896f2ea3efc7927371a1
Avagyan02 <samvel.avagyan.02@bk.ru>
2021-08-30 18:44:43 +0400

    registration_and_verify_4

--- api/auth/services/login.js
+++ api/auth/services/login.js
@@ -5,12 +5,14 @@ const generateUserToken = require('../token/JWT');
 
 function createUser(req,res){
   Users
-    .findOne({email: req.body.email, password: req.body.password})
+    .findOne({email: req.body.email})
     .then(result => {
       if(result){
         let pass = bcrypt.compareSync(req.body.password, result.password);
         if(pass){
           sendSuccessResponse(res, "Logged in", generateUserToken(result._id, result.email));
+        }else{
+          sendFailedResponse(res,"Incorrect email or password");
         }
       } else {
         sendFailedResponse(res,"Incorrect email or password");
