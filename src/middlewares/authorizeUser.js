ce8b18a22a8978853920a3f7e1b91e38f8c04afc
Avagyan02 <samvel.avagyan.02@bk.ru>
2021-09-26 20:52:06 +0400

    import babel in projetc 4

--- src/middlewares/authorizeUser.js
+++ src/middlewares/authorizeUser.js
@@ -1,26 +1,36 @@
 import jwt from 'jsonwebtoken';
+import User from '../models/user';
 import { HTTP_STATUS_CODE } from '../utils/constants';
 import { sendFailedResponse, sendErrorResponse } from '../utils/responseHelpers';
 import secret from '../api/auth/token/config';
-import findByPayload from '../utils/findByPayload';
 
-function authorizeUser(req, res, next) {
-  const message = 'Not valid token';
+async function authorizeUser(req, res, next) {
+  try {
+    const message = 'Not valid token';
+    let id;
 
-  if (req.headers.authorization) {
-    const token = req.headers.authorization.split(' ')[1];
-    if (!token) {
-      return sendFailedResponse(res, message, HTTP_STATUS_CODE.NOT_AUTHORIZED);
-    }
-    jwt.verify(token, secret.key, (err, payload) => {
-      if (err) {
-        console.log(payload, token, secret, err);
+    if (req.headers.authorization) {
+      const token = req.headers.authorization.split(' ')[1];
+      if (!token) {
         return sendFailedResponse(res, message, HTTP_STATUS_CODE.NOT_AUTHORIZED);
       }
-      findByPayload(req, res, next, payload.id);
-    });
-  } else {
-    sendFailedResponse(res, 'User is not logged in', HTTP_STATUS_CODE.NOT_AUTHORIZED);
+      jwt.verify(token, secret.key, (err, payload) => {
+        if (err) {
+          console.log(payload, token, secret, err);
+          return sendFailedResponse(res, message, HTTP_STATUS_CODE.NOT_AUTHORIZED);
+        } else {
+          id = payload.id;
+        }
+      });
+
+      const user = await User.findOne({ _id: id });
+      req.user = user;
+      next();
+    } else {
+      sendFailedResponse(res, 'User is not logged in', HTTP_STATUS_CODE.NOT_AUTHORIZED);
+    }
+  } catch (error) {
+    sendErrorResponse(error, res);
   }
 }
 
