19186c87e8cb7a64803a93e7d234673918ad61c1
Avagyan02 <samvel.avagyan.02@bk.ru>
2021-09-29 15:46:38 +0400

    auth middleware

--- src/middlewares/authorize.js
+++ src/middlewares/authorize.js
@@ -4,39 +4,69 @@ import { HTTP_STATUSES } from '../utils/constants';
 import { sendFailedResponse, sendErrorResponse } from '../utils/responseHelpers';
 import secret from '../api/auth/token/config';
 
-async function authorize(req, res, next) {
-  try {
-    let id;
-    let role;
+function checkToken(req, res) {
+  if (req.headers.authorization) {
+    const auth = req.headers.authorization.split(' ');
+    console.log(auth);
+    if (auth[0] === 'Bearer' && auth[1]) {
+      const token = auth[1];
 
-    if (req.headers.authorization) {
-      const auth = req.headers.authorization.split(' ');
-      if (auth[0] === 'Bearer' && auth[1]) {
-        const token = auth[1];
-
-        jwt.verify(token, secret.key, (err, payload) => {
-          if (err) {
-            return sendFailedResponse(res, HTTP_STATUSES.BAD_REQUEST.message, HTTP_STATUSES.BAD_REQUEST.code);
-          } else {
-            id = payload.id;
-            role = payload.role;
-          }
-        });
-
-        const user = await User.findOne({ _id: id, role });
-        if (!user) {
-          return sendFailedResponse(res, HTTP_STATUSES.BAD_REQUEST.message, HTTP_STATUSES.BAD_REQUEST.code);
+      jwt.verify(token, secret.key, (err, payload) => {
+        if (err) {
+          return sendFailedResponse(res, HTTP_STATUSES.BAD_REQUEST.message);
         }
-        req.user = user;
-        next();
-      } else {
-        return sendFailedResponse(res, HTTP_STATUSES.BAD_REQUEST.message, HTTP_STATUSES.BAD_REQUEST.code);
-      }
+        const userParams = [payload.id, payload.role];
+        console.log(userParams);
+        // userParams.push(payload.id, payload.role);
+        return { role: payload.role, id: payload.id };
+      });
     } else {
-      next();
+      return sendFailedResponse(res, HTTP_STATUSES.BAD_REQUEST.message);
     }
-  } catch (error) {
-    sendErrorResponse(error, res);
+  } else {
+    return sendFailedResponse(res, HTTP_STATUSES.BAD_REQUEST.message);
+  }
+}
+function authorize(req, res, next) {
+  const params = checkToken(req, res);
+  console.log(params);
+  if (typeof params === 'object' && params.role === 1) {
+    return async function () {
+      try {
+        //  const user = checkToken(req, res);
+
+        // if (user[1] !== 1) {
+        //   return sendFailedResponse(res, HTTP_STATUSES.FORBIDDEN.message);
+        // }
+        const admin = await User.findOne({ _id: params.id, role: params.role });
+        if (!admin) {
+          return sendFailedResponse(res, HTTP_STATUSES.BAD_REQUEST.message);
+        }
+        req.user = admin;
+        next();
+      } catch (error) {
+        sendErrorResponse(error, res);
+      }
+    };
+  } else if (Array.isArray(params) && params[1] === 2) {
+    return async function () {
+      try {
+        const user = checkToken(req, res);
+        console.log(user);
+
+        const findUser = await User.findOne({ _id: params.id, role: params.role });
+        if (!findUser) {
+          return sendFailedResponse(res, HTTP_STATUSES.BAD_REQUEST.message);
+        }
+        req.user = findUser;
+        next();
+      } catch (error) {
+        sendErrorResponse(error, res);
+      }
+    };
+  } else {
+    console.log(params);
+    return sendFailedResponse(res, HTTP_STATUSES.BAD_REQUEST.message);
   }
 }
 
