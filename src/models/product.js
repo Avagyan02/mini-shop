115ca0b5a0e7848657cc4f332c16182d5a20af59
Avagyan02 <samvel.avagyan.02@bk.ru>
2021-10-02 17:02:49 +0400

    add product

---
+++ src/models/product.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+  nameEn: {
+    type: String,
+    required: true,
+  },
+  nameRu: {
+    type: String,
+    required: true,
+  },
+  nameHy: {
+    type: String,
+    required: true,
+  },
+  category_id: {
+    type: String,
+    required: 'Category is required',
+  },
+  createDt: {
+    type: Date,
+    default: Date.now,
+  },
+  updateDt: {
+    type: Date,
+    default: Date.now,
+  },
+  quantity: {
+    type: Number,
+    default: 1,
+    required: 'Quantity is required',
+  },
+  image: {
+    type: String,
+    required: 'Image is required',
+  },
+  price: {
+    type: Number,
+    required: 'Price is required',
+  },
+  description: {
+    type: String,
+    required: 'Description is required',
+  },
+});
+
+const Prod = mongoose.model('product_collections', productSchema);
+export default Prod;
