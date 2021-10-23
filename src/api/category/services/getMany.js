6bfcacc3ccf6d6a8e3df2a2a2d67ca396e0d9425
Avagyan02 <samvel.avagyan.02@bk.ru>
2021-10-23 20:53:46 +0400

    change categoryList

--- src/api/category/services/getMany.js
+++ src/api/category/services/getMany.js
@@ -1,35 +1,26 @@
 import Category from '../../../models/category';
-import { USER_ROLES } from '../../../utils/constants';
+import { HTTP_STATUSES, USER_ROLES } from '../../../utils/constants';
 import { sendSuccessResponse, sendErrorResponse, sendFailedResponse } from '../../../utils/responseHelpers';
 
 async function readMany(req, res) {
   try {
-    const categoryCount = await Category.countDocuments();
     const limit = +req.query.limit;
     const pageNo = +req.query.pageNo;
-    const categoryListQuant = categoryCount < limit * pageNo;
-    const pageCount = Math.ceil(categoryCount / limit);
     const filter = { deleted: false };
     const message = 'Category list fetched';
-
-    if (!categoryCount) {
-      return sendSuccessResponse(res, message, {
-        count: categoryCount,
-        pageCount: 1,
-        list: [],
-      });
-    } else if (categoryListQuant) {
-      return sendFailedResponse(res, 'It is not possible to split into so many elements');
-    }
 
     if (!req.user || req.user.role === USER_ROLES.user) {
       filter.productCount = { $gte: 1 };
     }
-    const categoryList = await Category.find(filter).skip((pageNo - 1) * limit).limit(limit);
+
+    const filteredCategory = await Category.find(filter);
+    if (pageNo * limit > filteredCategory.length) {
+      return sendFailedResponse(res, 'It is not possible to split into so many elements');
+    }
     return sendSuccessResponse(res, message, {
-      count: categoryList.length,
-      pageCount,
-      list: categoryList,
+      count: filteredCategory.length,
+      pageCount: Math.ceil(filteredCategory.length / limit),
+      list: filteredCategory.slice((pageNo * limit) - 1, (pageNo + 1) * limit),
     });
   } catch (error) {
     sendErrorResponse(error, res);
