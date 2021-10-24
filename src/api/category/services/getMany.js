0aae03032cd89ccb60484a0c4d6f64304172b347
Avagyan02 <samvel.avagyan.02@bk.ru>
2021-10-24 18:17:33 +0400

    change received product and category 3

--- src/api/category/services/getMany.js
+++ src/api/category/services/getMany.js
@@ -1,29 +1,19 @@
 import Category from '../../../models/category';
-import { HTTP_STATUSES, USER_ROLES } from '../../../utils/constants';
-import { sendSuccessResponse, sendErrorResponse, sendFailedResponse } from '../../../utils/responseHelpers';
+import { USER_ROLES } from '../../../utils/constants';
+import sendPaginatedList from '../../../utils/sendPaginatedList';
+import { sendErrorResponse } from '../../../utils/responseHelpers';
 
 async function readMany(req, res) {
   try {
     const limit = +req.query.limit;
     const pageNo = +req.query.pageNo;
     const filter = { deleted: false };
-    const message = 'Category list fetched';
 
     if (!req.user || req.user.role === USER_ROLES.user) {
       filter.productCount = { $gte: 1 };
     }
 
-    const filteredCategoryCount = await Category.countDocuments(filter);
-    if (pageNo * limit > filteredCategoryCount) {
-      return sendFailedResponse(res, 'It is not possible to split into so many elements');
-    }
-
-    const filteredCategory = await Category.find(filter).skip((pageNo - 1) * limit).limit(limit);
-    return sendSuccessResponse(res, message, {
-      count: filteredCategoryCount,
-      pageCount: Math.ceil(filteredCategoryCount / limit),
-      list: filteredCategory,
-    });
+    return sendPaginatedList(res, Category, filter, pageNo, limit);
   } catch (error) {
     sendErrorResponse(error, res);
   }
--- src/api/product/services/getMany.js
+++ src/api/product/services/getMany.js
@@ -1,24 +1,14 @@
 import Product from '../../../models/product';
-import { sendSuccessResponse, sendFailedResponse, sendErrorResponse } from '../../../utils/responseHelpers';
+import sendPaginatedList from '../../../utils/sendPaginatedList';
+import { sendErrorResponse } from '../../../utils/responseHelpers';
 
 async function readMany(req, res) {
   try {
     const limit = +req.query.limit;
     const pageNo = +req.query.pageNo;
     const filter = { deleted: false };
-    const message = 'Product list fetched';
 
-    const filteredProductCount = await Product.countDocuments(filter);
-    if (limit * pageNo > filteredProductCount) {
-      return sendFailedResponse(res, 'It is not possible to split into so many elements');
-    }
-
-    const filteredProduct = await Product.find(filter).skip((pageNo - 1) * limit).limit(limit);
-    return sendSuccessResponse(res, message, {
-      count: filteredProductCount,
-      pageCount: Math.ceil(filteredProductCount / pageNo),
-      list: filteredProduct,
-    });
+    return sendPaginatedList(res, Product, filter, pageNo, limit);
   } catch (error) {
     sendErrorResponse(error, res);
   }
